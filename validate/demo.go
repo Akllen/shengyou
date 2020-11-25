322580b8a8a656fb15c3900449350749a76e11d1
huanghe <651457780@qq.com>
2020-11-25 09:43:01 +0800

    no message

--- validate/demo.go
+++ validate/demo.go
@@ -1,15 +1,24 @@
 /*
  * @Author: 黄河
  * @Date: 2020-11-25 09:35:50
- * @LastEditTime: 2020-11-25 09:36:26
+ * @LastEditTime: 2020-11-25 09:41:33
  * @LastEditors: 黄河
  */
 package validate
 
 import (
 	"fmt"
+
+	"github.com/go-playground/validator/v10"
 )
 
 func DemoTest() {
 	fmt.Println("Demo Test")
+}
+
+func CheckSruct(data interface{}) error {
+	if err := validator.New().Struct(data); err != nil {
+		return err
+	}
+	return nil
 }
