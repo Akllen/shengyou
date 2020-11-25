5a6408e9207717805eceb1983bd2a0292e5b59be
huanghe <651457780@qq.com>
2020-11-25 09:38:29 +0800

    no message

---
+++ utils/demo.go
@@ -0,0 +1,15 @@
+/*
+ * @Author: 黄河
+ * @Date: 2020-11-25 09:32:35
+ * @LastEditTime: 2020-11-25 09:33:27
+ * @LastEditors: 黄河
+ */
+package utils
+
+import (
+	"fmt"
+)
+
+func Demo() {
+	fmt.Println("Hello Demo")
+}
---
+++ validate/demo.go
@@ -0,0 +1,15 @@
+/*
+ * @Author: 黄河
+ * @Date: 2020-11-25 09:35:50
+ * @LastEditTime: 2020-11-25 09:36:26
+ * @LastEditors: 黄河
+ */
+package validate
+
+import (
+	"fmt"
+)
+
+func DemoTest() {
+	fmt.Println("Demo Test")
+}
